8364b210daf852f193af85d093de6fdded68a4ad
ohadshai@gmail.com <ohadshai@gmail.com@b633b239-4e2e-4ba4-29ec-451c36174299>
2012-11-18 17:10:56 +0000

    added unit tests

--- trunk/simba/java/junit/sim/scheduling/matchers/DegreeMixFitTest.java
+++ trunk/simba/java/junit/sim/scheduling/matchers/DegreeMixFitTest.java
@@ -1,60 +1,45 @@
 package sim.scheduling.matchers;
 
-import static com.google.common.collect.Lists.*;
+import static org.junit.Assert.*;
 
-import java.util.List;
-
-import org.junit.Ignore;
 import org.junit.Test;
 
 import sim.model.Host;
+import sim.model.Job;
+import sim.scheduling.graders.Grader;
 
 public class DegreeMixFitTest
 {
 
-	@Ignore
 	@Test
-	public void testNoAccpetingHostNotFit()
+	public void testPerfectFit()
 	{
-		List<Host> hosts = newArrayList(Host.create().build());
-		// assertNull(GradeMatcherProvider.createGraderMf1().match(Job.create(1).cores(1).build(),
-		// hosts));
+		Host host = Host.create().cores(1).memory(1).build();
+		Job job = Job.create(1).cores(1).memory(1).build();
+		Grader grader = GradeMatcherProvider.createGraderMf1();
+		assertEquals(0, grader.getGrade(host, job), 0.1);
 	}
 
-	@Ignore
 	@Test
-	public void testAccpetingHostFit()
+	public void testMainstream()
 	{
-		Host host = Host.create().cores(1).build();
-		List<Host> hosts = newArrayList(host);
-		// assertEquals(host,
-		// GradeMatcherProvider.createGraderMf1().match(Job.create(1).cores(1).build(),
-		// hosts));
+		Host host = Host.create().cores(1).memory(8).build();
+		Job job1 = Job.create(1).cores(1).memory(4).build();
+		Job job2 = Job.create(1).cores(1).memory(2).build();
+		Grader grader = GradeMatcherProvider.createGraderMf1();
+		assertTrue(grader.getGrade(host, job1) > grader.getGrade(host, job2));
 	}
 
-	@Ignore
 	@Test
-	public void testAccpetingBestHost()
+	public void testUsedHost()
 	{
-		Host host1 = Host.create().cores(2).memory(16).build();
-		Host host2 = Host.create().cores(2).memory(15).build();
-		Host host3 = Host.create().cores(2).memory(8).build();
-		List<Host> hosts = newArrayList(host1, host2, host3);
-		// assertEquals(host3,
-		// GradeMatcherProvider.createGraderMf1().match(Job.create(1).cores(1).memory(4).build(),
-		// hosts));
-	}
-
-	@Ignore
-	@Test
-	public void testWhenEqualsUsingLessAvailableMemoryHost()
-	{
-		Host host1 = Host.create().cores(2).memory(16).build();
-		Host host2 = Host.create().cores(1).memory(8).build();
-		List<Host> hosts = newArrayList(host1, host2);
-		// assertEquals(host2,
-		// GradeMatcherProvider.createGraderMf1().match(Job.create(1).cores(1).memory(4).build(),
-		// hosts));
+		Host host = Host.create().cores(2).memory(16).build();
+		Job job1 = Job.create(1).cores(1).memory(4).build();
+		Job job2 = Job.create(1).cores(1).memory(8).build();
+		Job job3 = Job.create(1).cores(1).memory(12).build();
+		host.dispatchJob(job1);
+		Grader grader = GradeMatcherProvider.createGraderMf1();
+		assertTrue(grader.getGrade(host, job3) > grader.getGrade(host, job2));
 	}
 
 }
