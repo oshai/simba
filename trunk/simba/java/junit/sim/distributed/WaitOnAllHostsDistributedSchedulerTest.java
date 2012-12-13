393df968a7ba7d19f4782f31ad475fed08200517
ohadshai@gmail.com <ohadshai@gmail.com@b633b239-4e2e-4ba4-29ec-451c36174299>
2012-12-13 20:47:20 +0000

    improved coverage

--- trunk/simba/java/junit/sim/distributed/WaitOnAllHostsDistributedSchedulerTest.java
+++ trunk/simba/java/junit/sim/distributed/WaitOnAllHostsDistributedSchedulerTest.java
@@ -2,16 +2,17 @@ package sim.distributed;
 
 import static org.mockito.Mockito.*;
 
-import java.util.*;
+import java.util.ArrayList;
 
-import org.junit.*;
-import org.junit.runner.*;
-import org.mockito.runners.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import sim.model.*;
-import sim.scheduling.*;
+import sim.model.Job;
+import sim.scheduling.LinkedListWaitingQueue;
+import sim.scheduling.SetWaitingQueue;
 
-import com.google.common.collect.*;
+import com.google.common.collect.Lists;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WaitOnAllHostsDistributedSchedulerTest
@@ -25,12 +26,25 @@ public class WaitOnAllHostsDistributedSchedulerTest
 		LinkedListWaitingQueue waitingQueue = new LinkedListWaitingQueue();
 		Job job = mock(Job.class);
 		waitingQueue.add(job);
-		WaitOnAllHostsDistributedScheduler tested = new WaitOnAllHostsDistributedScheduler(waitingQueue, hostSchedulers, new SetWaitingQueue());
+		WaitOnAllHostsDistributedScheduler tested = createScheduler(hostSchedulers, waitingQueue);
 		tested.distributeJobs(time);
 		for (HostScheduler hostScheduler : hostSchedulers)
 		{
 			verify(hostScheduler).addJob(job);
 		}
+	}
+
+	private WaitOnAllHostsDistributedScheduler createScheduler(ArrayList<HostScheduler> hostSchedulers, LinkedListWaitingQueue waitingQueue)
+	{
+		WaitOnAllHostsDistributedScheduler tested = new WaitOnAllHostsDistributedScheduler(waitingQueue, hostSchedulers, new SetWaitingQueue());
+		return tested;
+	}
+
+	@Test
+	public void testDoNothing()
+	{
+		WaitOnAllHostsDistributedScheduler tested = createScheduler(null, null);
+		tested.scheduleWaitingJobsAgain(0);
 	}
 
 	private ArrayList<HostScheduler> createHostSchedulers(int size)
