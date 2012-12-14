610aa2cf3ca2eeedd51b7e372f237ea83130374e
ohadshai@gmail.com <ohadshai@gmail.com@b633b239-4e2e-4ba4-29ec-451c36174299>
2012-12-14 01:20:04 +0000

    added emma coverage -  check %

--- simba/test_coverage.sh
+++ simba/test_coverage.sh
@@ -1,1 +1,1 @@
-test `grep "coverage type=\"line" coverage.xml | head -1 | awk -F\" '{print $4}' | awk '{print $1}'` == '100%'
+test `grep "coverage type=\"line" /var/lib/jenkins/jobs/simba/workspace/coverage.xml | head -1 | awk -F\" '{print $4}' | awk '{print $1}'` == '100%'
