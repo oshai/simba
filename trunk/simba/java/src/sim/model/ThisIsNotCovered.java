dd712efdad51a0ea391e655b6b1f712a9716287d
ohadshai@gmail.com <ohadshai@gmail.com@b633b239-4e2e-4ba4-29ec-451c36174299>
2012-12-14 00:47:24 +0000

    added emma coverage -  check %

---
+++ trunk/simba/java/src/sim/model/ThisIsNotCovered.java
@@ -0,0 +1,18 @@
+package sim.model;
+
+public class ThisIsNotCovered
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+}
