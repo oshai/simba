04950a0c09796b79f1fccbfe5180603ce45da229
ohadshai@gmail.com <ohadshai@gmail.com@b633b239-4e2e-4ba4-29ec-451c36174299>
2012-12-14 01:57:52 +0000

    added emma coverage -  try to fix coverage %

---
+++ simba/java/src/sim/model/AAA.java
@@ -0,0 +1,35 @@
+package sim.model;
+
+public class AAA
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+}
