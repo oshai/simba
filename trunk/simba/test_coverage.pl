3e3bb7c5abac93cda3fc43a59341c675523fe5b2
ohadshai@gmail.com <ohadshai@gmail.com@b633b239-4e2e-4ba4-29ec-451c36174299>
2012-12-14 01:49:05 +0000

    added emma coverage -  check %

--- trunk/simba/test_coverage.pl
+++ trunk/simba/test_coverage.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-my $coverage = `grep 'coverage type=\"line' coverage.xml | head -1 | awk -F\" '{print $4}' | awk '{print \$1}'`;
+my $coverage = `grep 'coverage type=\"line' coverage.xml | head -1 | awk -F\\" '{print $4}' | awk '{print \$1}'`;
 chomp($coverage);
 print "coverage of lines is $coverage\n";
 exit ($coverage eq '100%' ? 0 : 1);
