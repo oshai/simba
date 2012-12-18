ca6298ff24957de974c092b5c74a32ff34a98d25
ohadshai@gmail.com <ohadshai@gmail.com@b633b239-4e2e-4ba4-29ec-451c36174299>
2012-12-18 06:55:56 +0000

    add collision detection

--- trunk/simba/java/src_no_junit/sim/CollisionDetection.java
+++ trunk/simba/java/src_no_junit/sim/CollisionDetection.java
@@ -1,5 +1,7 @@
 package sim;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -8,33 +10,43 @@ import utils.TextFileUtils;
 
 public class CollisionDetection
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		new CollisionDetection().execute();
 	}
 
-	private void execute()
+	private void execute() throws IOException
 	{
 		// int numOfJobs = 1;
+		for (int i = 1; i < 20; i++)
+		{
+			System.out.println("test " + i);
+			printForRetry(i);
+		}
+	}
+
+	private void printForRetry(int retries) throws IOException
+	{
 		int numOfMachines = 14000;
 		String result = "";
 		for (int i = 1; i <= numOfMachines; i++)
 		{
-			int collisions = calcCollisions(i, numOfMachines);
+			int collisions = calcCollisions(i, numOfMachines, retries);
 			result += i + " " + collisions + "\n";
-
 		}
-		TextFileUtils.setContents("collisions", result);
+		String file = "collisions_" + retries;
+		new File(file).createNewFile();
+		TextFileUtils.setContents(file, result);
 	}
 
-	private int calcCollisions(int numOfJobs, int numOfMachines)
+	private int calcCollisions(int numOfJobs, int numOfMachines, int retries)
 	{
 		Set<Integer> machines = new HashSet<Integer>();
 		Random random = new Random();
 		int collisions = 0;
 		for (int i = 0; i < numOfJobs; i++)
 		{
-			int machine = random.nextInt(numOfMachines);
+			int machine = findMachine(numOfMachines, random, machines, retries);
 			if (machines.contains(machine))
 			{
 				collisions++;
@@ -46,5 +58,19 @@ public class CollisionDetection
 
 		}
 		return collisions;
+	}
+
+	private int findMachine(int numOfMachines, Random random, Set<Integer> machines, int retries)
+	{
+		int machine = 0;
+		for (int i = 0; i < retries; i++)
+		{
+			machine = random.nextInt(numOfMachines);
+			if (!machines.contains(machine))
+			{
+				return machine;
+			}
+		}
+		return machine;
 	}
 }
