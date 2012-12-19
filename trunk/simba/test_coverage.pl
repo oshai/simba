d0609ff66e4f8e8896c27f78dd3c7afd2ace36fd
ohadshai@gmail.com <ohadshai@gmail.com@b633b239-4e2e-4ba4-29ec-451c36174299>
2012-12-19 20:16:41 +0000

    improved coverage

--- trunk/simba/test_coverage.pl
+++ trunk/simba/test_coverage.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-my $coverage = `awk -F, '{print \$4}' target/site/jacoco/report.csv | grep -v INSTRUCTION_MISSED | grep -v '0'`;
+my $coverage = `awk -F, '{print \$3,\$4}' target/site/jacoco/report.csv | grep -v INSTRUCTION_MISSED | grep -v '0'`;
 chomp($coverage);
-print "coverage of lines is: $coverage\n";
+print "coverage of lines is:\n$coverage\n";
 exit ($coverage eq '' ? 0 : 1);
--- trunk/simba/test_coverage.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo coverage of lines `grep "coverage type=\"line" coverage.xml | head -1 | awk -F\" '{print $4}' | awk '{print $1}'`
-test `grep "coverage type=\"line" coverage.xml | head -1 | awk -F\" '{print $4}' | awk '{print $1}'` == '100%'
